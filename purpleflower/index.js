bc3e822a475e32e1970bafcf429c17ecea13ac27
abdu <abdualkerim01@gmail.com>
2026-02-22 11:14:38 +0300

    p element added

--- purpleflower/index.js
+++ purpleflower/index.js
@@ -1,7 +1,20 @@
 const purpleFlower = document.getElementById("container");
-// i make the below long code but not good
-// let text = purpleFlower.textContent = "Buy !"
-// purpleFlower.innerHTML = "<button>" + text + "</button>"
+const thankPa = document.getElementById("thanks")
+    // i make the below long code but not good
+    // let text = purpleFlower.textContent = "Buy !"
+    // purpleFlower.innerHTML = "<button>" + text + "</button>"
 
 // i can write it like
-purpleFlower.innerHTML = "<button>Buyer!</button>";
+purpleFlower.innerHTML = "<button onclick='buy()'>Bye!</button>";
+
+
+// the below code works after creating p element in html
+purpleFlower.addEventListener("click", function() {
+    thankPa.textContent = "shukren"
+})
+
+// without p elelment
+
+function buy() {
+    container.innerHTML += "<p>peace</p>"
+}
