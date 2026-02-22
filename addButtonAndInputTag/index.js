abf5a454166867e82f42947ae66151d5066de3ba
abdu <abdualkerim01@gmail.com>
2026-02-22 10:14:25 +0300

    push value from input field

--- addButtonAndInputTag/index.js
+++ addButtonAndInputTag/index.js
@@ -8,6 +8,7 @@ const savBtn = document.getElementById("input-btn");
 // so we dont need to add onclick attribute in html
 // the below is professional way
 savBtn.addEventListener("click", function() {
-    myLeads.push("www.awesomelead.com");
+    inputItem = inputEl.value
+    myLeads.push(inputItem);
     console.log(myLeads);
 });
