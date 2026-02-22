18474a9e51009de06343ffe12a0061be510d7f4d
abdu <abdualkerim01@gmail.com>
2026-02-22 10:57:32 +0300

    buyer button added

---
+++ purpleflower/index.js
@@ -0,0 +1,7 @@
+const purpleFlower = document.getElementById("container");
+// i make the below long code but not good
+// let text = purpleFlower.textContent = "Buy !"
+// purpleFlower.innerHTML = "<button>" + text + "</button>"
+
+// i can write it like
+purpleFlower.innerHTML = "<button>Buyer!</button>";
