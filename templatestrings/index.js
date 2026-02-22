2ad7b5678591b551014beab0a663395b47a83e3d
abdu <abdualkerim01@gmail.com>
2026-02-22 14:11:57 +0300

    using more of template strings

--- templatestrings/index.js
+++ templatestrings/index.js
@@ -1,5 +1,6 @@
 //tenplate strigns example
 const recipient = "James";
+const sender = "Abdu";
 // const email = "Hey " + recipient + "! How is it going? Cheers Per";
-const email = `Hey ${recipient}! How is it going? Cheers Per`;
+const email = `Hey ${recipient}! How is it going? Cheers ${sender}!`;
 console.log(email);
