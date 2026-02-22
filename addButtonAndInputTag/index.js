dc39fee66a48a514707d4beab35094331b39d36f
abdu <abdualkerim01@gmail.com>
2026-02-22 10:34:14 +0300

    render listes in unorderlist

--- addButtonAndInputTag/index.js
+++ addButtonAndInputTag/index.js
@@ -1,12 +1,13 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const savBtn = document.getElementById("input-btn");
-
-// function saveInput() {
-//     console.log("Button Clicked from onclick attribute")
-// }
-// so we dont need to add onclick attribute in html
-// the below is professional way
+const ulEl = document.getElementById("ul-el")
+console.log(ulEl)
+    // function saveInput() {
+    //     console.log("Button Clicked from onclick attribute")
+    // }
+    // so we dont need to add onclick attribute in html
+    // the below is professional way
 savBtn.addEventListener("click", function() {
     inputItem = inputEl.value
     myLeads.push(inputItem);
@@ -14,5 +15,5 @@ savBtn.addEventListener("click", function() {
 });
 
 for (let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
+    ulEl.textContent += myLeads[i] + " "
 }
