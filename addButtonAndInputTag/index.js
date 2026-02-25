1d767e9933cacbb35d13cd15ec1077ff8f78f34b
abdu <abdualkerim01@gmail.com>
2026-02-25 11:23:09 +0300

    chrome extention woked out

--- addButtonAndInputTag/index.js
+++ addButtonAndInputTag/index.js
@@ -1,38 +1,47 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const savBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el")
-console.log(ulEl)
-    // function saveInput() {
-    //     console.log("Button Clicked from onclick attribute")
-    // }
-    // so we dont need to add onclick attribute in html
-    // the below is professional way
-savBtn.addEventListener("click", function() {
-    inputItem = inputEl.value
-    myLeads.push(inputItem);
-    inputEl.value = ""
-        // console.log(myLeads);
-    renderLeads();
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+}
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    });
 });
 
-
-
-
-// 1. Create a variable, listItems, to hold all the HTML for the list items
-// Assign it to an empty string to begin with
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
-        // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-        // but the below ia more good
-        // listItems += "<li><a href='#'>" + myLeads[i] + "</a></li>";
-        // using template string
+function render(leads) {
+    let listItems = "";
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
-        <li><a href='${myLeads[i]}'>${myLeads[i]}</a></li>
- `;
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `;
     }
-
-    ulEl.innerHTML = listItems
+    ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+});
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+});
